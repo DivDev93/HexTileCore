1e47983c3c84bc4dda492f0d0fb4b15c26e3215b
DivDev93 <106773169+DivDev93@users.noreply.github.com>
2025-02-14 11:51:51 -0500

    Improvements to camera control input logic with functionality for two finger drag

--- Scripts/Miscellaneous/CameraZoomController.cs
+++ Scripts/Miscellaneous/CameraZoomController.cs
@@ -20,42 +20,43 @@ public class CameraZoomController : MonoBehaviour
     public float pitchMax = 30f;
     public float panFactor = 20f;
 
-    //public InputActionReference zoomActionRef;
-    //public InputAction zoomAction => screenSpaceInputs.screenSpacePinchScaleInput.pinchGapDeltaInput.inputAction;//zoomActionRef.action;
-
-    //public InputActionReference panActionRef;
-    //public InputAction panAction => screenSpaceInputs.screenSpaceRotateInput.dragDeltaInput.inputAction;
     Transform parent;
 
     private void OnEnable()
     {
-        //zoomAction.Enable();
-        //panAction.Enable();
         parent = transform.parent;
     }
-
-    //private void OnDisable()
-    //{
-    //    zoomAction.Disable();
-    //    panAction.Disable();
-    //}
 
     void UpdateZoom()
     {
         float zoomDelta = screenSpaceInputs.screenSpacePinchScaleInput.pinchGapDeltaInput.ReadValue() * zoomSensitivity;//.deltaPosition.y
-        //zoomCurrent = Vector3.Distance(Vector3.zero, transform.position);//Mathf.Clamp(zoomCurrent - zoomDelta * zoomSpeed * Time.fixedDeltaTime, zoomMin, zoomMax);
         Vector3 newPos = transform.position + transform.forward * zoomDelta * zoomSpeed * Time.fixedDeltaTime;
         zoomCurrent = Vector3.Distance(Vector3.zero, newPos);
-        if (zoomCurrent < zoomMin || zoomCurrent > zoomMax)
+        if (newPos.y < 0 || zoomCurrent < zoomMin || zoomCurrent > zoomMax)
             return;
 
+        //Debug.Log("zoom delta is " + zoomDelta + " zoom current is " + zoomCurrent);
         transform.position = newPos;
     }
 
     public float previousPitch = 0;
-    void UpdatePitch()
+    bool UpdatePitch()
     {
+#if UNITY_ANDROID
+        if (!Application.isEditor && screenSpaceInputs.screenSpaceRotateInput.screenTouchCountInput.ReadValue() < 2)
+        {
+            //Debug.Log("touch count is " + screenSpaceInputs.screenSpaceRotateInput.screenTouchCountInput.ReadValue());
+            return false;
+        }
+#endif
+
         Vector2 pitchDelta = screenSpaceInputs.screenSpaceRotateInput.dragDeltaInput.ReadValue() * panFactor;
+        
+        if(pitchDelta == Vector2.zero)
+        {
+            return false;
+        }
+
         //Debug.Log("parent x rot is " + parent.eulerAngles.x + " pitch delta is " + pitchDelta);
         float adjustedAngle = parent.eulerAngles.x > 180 ? parent.eulerAngles.x - 360 : parent.eulerAngles.x;
         float newPitch = adjustedAngle + pitchDelta.y * Time.fixedDeltaTime;
@@ -80,26 +81,15 @@ public class CameraZoomController : MonoBehaviour
             previousPitch = parent.eulerAngles.x;
             parent.rotation = Quaternion.Euler(parent.eulerAngles.With(x: newPitch,
           y: parent.eulerAngles.y + pitchDelta.x * Time.fixedDeltaTime));
+            return true;
         }
 
-       
-
-        //if (pitchDelta.y > pitchDelta.x)
-        //    parent.rotation = Quaternion.Euler(parent.eulerAngles.With(x: transform.parent.eulerAngles.x + pitchDelta.y));
-        //else
-        //    parent.rotation = Quaternion.Euler(parent.eulerAngles.With(y: transform.parent.eulerAngles.y + pitchDelta.x));
-
-        //float pitch = transform.parent.eulerAngles.x - pitchDelta;
-        //pitch = Mathf.Clamp(pitch, pitchMin, pitchMax);
-        //Debug.Log("delta and new pitch are " + pitchDelta + " " + pitch);
-        //transform.parent.eulerAngles = new Vector3(pitch, transform.parent.eulerAngles.y, transform.parent.eulerAngles.z);
-
+        return false;
     }
 
     private void FixedUpdate()
     {
-        UpdateZoom();
-        UpdatePitch();
+        if(!UpdatePitch())
+            UpdateZoom();
     }
-
 }
